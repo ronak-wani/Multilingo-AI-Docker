6db8dcc7e0d0e018ffc475f4d2d799719c469a87
Ronak Wani <117783872+ronak-wani@users.noreply.github.com>
2024-11-10 22:08:36 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,13 +5,13 @@ apt-get update && apt-get upgrade -y
 apt-get install -yq --no-install-recommends apt-utils
 apt-get upgrade -yq ca-certificates
 apt-get install -yq --no-install-recommends curl prometheus-node-exporter
-#echo "Ollama is not installed. Installing now..."
-#curl -fsSL https://ollama.com/install.sh | sh
-#chmod +x local_serve.sh
-#./local_serve.sh
-#echo "Waiting for key generation..."
-#sleep 5
-#ollama pull mistral
+echo "Ollama is not installed. Installing now..."
+curl -fsSL https://ollama.com/install.sh | sh
+chmod +x local_serve.sh
+./local_serve.sh
+echo "Waiting for key generation..."
+sleep 5
+ollama pull mistral
 pip install -r requirements.txt
 echo "Grafana Begins"
 
@@ -37,16 +37,3 @@ echo "Grafana installation Completed"
 /usr/sbin/grafana-server --config=/etc/grafana/grafana.ini --homepath=/usr/share/grafana --packaging=docker &
 
 echo "Grafana Server Started"
-
-### Install Ngrok
-#curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc | tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null
-#echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | tee /etc/apt/sources.list.d/ngrok.list
-#apt-get update
-#apt-get install -y ngrok
-#
-## Configure Ngrok
-#echo "NGROK_TOKEN: $NGROK_TOKEN"
-#NGROK_TOKEN=$NGROK_TOKEN
-#ngrok config add-authtoken NGROK_TOKEN
-#ngrok http http://localhost:8010 &
-#ngrok http --log=ngrok_log.txt http://localhost:8010 &
